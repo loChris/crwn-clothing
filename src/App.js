eff240d596a456c909b0a8a4e28a30b3b6eac160
loChris <44450518+loChris@users.noreply.github.com>
2020-08-12 17:15:37 -0400

    Sass setup and code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import HomePage from './homepage.component';
 import './App.css';
 
 function App() {
-	return <div className="App">Hello world</div>;
+	return (
+		<div className="App">
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
