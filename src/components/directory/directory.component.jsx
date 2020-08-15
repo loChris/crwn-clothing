23607485cb6418e5b8a664140d494c14e135e1a4
loChris <44450518+loChris@users.noreply.github.com>
2020-08-15 12:45:44 -0400

    started shop page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -46,13 +46,8 @@ class Directory extends React.Component {
 	render() {
 		return (
 			<div className="directory-menu">
-				{this.state.sections.map(({ title, imageUrl, id, size }) => (
-					<MenuItem
-						title={title}
-						imageUrl={imageUrl}
-						key={id}
-						size={size}
-					/>
+				{this.state.sections.map(({ id, ...otherSetionProps }) => (
+					<MenuItem key={id} {...otherSetionProps} />
 				))}
 			</div>
 		);
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PreviewCollection from '../../components/preview-collection/preview-collection.component';
+import SHOP_DATA from './shop.data.js';
+
+export default class Shop extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			collections: SHOP_DATA,
+		};
+	}
+	render() {
+		const { collections } = this.state;
+		return (
+			<div className="shop-page">
+				{collections.map(({ id, ...otherCollectionProps }) => (
+					<PreviewCollection key={id} {...otherCollectionProps} />
+				))}
+			</div>
+		);
+	}
+}
