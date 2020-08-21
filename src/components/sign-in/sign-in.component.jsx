8f7bba8a8d361bc6097d3f5b2de14ba9ed05c1d9
loChris <44450518+loChris@users.noreply.github.com>
2020-08-21 14:07:28 -0400

    users can now sign in with their emails and passwords (other than sign in with google option)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -47,9 +47,16 @@ class SignIn extends React.Component {
 		);
 	}
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.setState({ email: '', password: '' });
+	handleSubmit = async (event) => {
+		const { email, password } = this.state;
+		event.preventDefault();
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	handleChange = (e) => {
