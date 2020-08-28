a4b9117e0904fbf9672518de4e747426db4b46d7
loChris <44450518+loChris@users.noreply.github.com>
2020-08-28 13:18:53 -0400

    implemented userReducer and userActions to the header component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
-	<Provider>
+	<Provider store={store}>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
